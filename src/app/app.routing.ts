5326cf7dfa2d2c20e0341abc46cf27a4757f8527
pmartinh <mh.patricio@gmail.com>
2018-03-08 20:16:26 +0100

    prueba

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,9 +3,11 @@ import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home.component';
 import {NotFoundComponent} from './notfound.component';
 import {GitHubComponent} from './github.component';
+import {GitHubUserComponent} from './github.user.component'
 
 export const routing = RouterModule.forRoot([
     {path:'', component:HomeComponent},
     {path:'GitHub', component:GitHubComponent},
-    {path:'**', component:NotFoundComponent}
+    {path:'**', component:NotFoundComponent},
+    {path:'GitHub/user/:login/:score', component:GitHubUserComponent}
 ])
