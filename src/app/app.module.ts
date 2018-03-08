f85ae3a0608fd81e5c0c48acf52972a341b2373f
pmartinh <mh.patricio@gmail.com>
2018-03-08 18:53:05 +0100

    params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { Http } from '@angular/http';
@@ -9,14 +9,15 @@ import { HomeComponent } from './home.component';
 import { GitHubComponent } from './github.component';
 import { NotFoundComponent } from './notfound.component';
 import { routing } from './app.routing';
+import { GitHubUserComponent } from './githubuser.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,HomeComponent,GitHubComponent,NotFoundComponent
+    AppComponent,HomeComponent,GitHubComponent,NotFoundComponent, GitHubUserComponent
   ],
   imports: [
-    BrowserModule,HttpModule,ReactiveFormsModule,routing
+    BrowserModule,HttpModule,ReactiveFormsModule,routing,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,20 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './home.component';
-import {NotFoundComponent} from './notfound.component';
-import {GitHubComponent} from './github.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './notfound.component';
+import { GitHubComponent } from './github.component';
+import { GitHubUserComponent } from './githubuser.component';
+
+//import { LoginComponent } from './login.component';
+//import { AuthGuard } from './auth-guard.service';
+
+//import { PreventUnsavedChangesGuard } from './prevent-unsaved-changes-guard.service';
 
 export const routing = RouterModule.forRoot([
-    {path:'', component:HomeComponent},
-    {path:'GitHub', component:GitHubComponent},
-    {path:'**', component:NotFoundComponent}
-])
+  {path: '', component: HomeComponent},
+  {path: 'GitHub', component: GitHubComponent},
+  {path: 'GitHub/user/:login/:score', component: GitHubUserComponent},
+ // {path: 'login', component: LoginComponent,},
+    {path: '**', component: NotFoundComponent}
+]);
