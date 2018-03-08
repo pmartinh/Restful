60a7c4a69cff6d6e364f44d90e62c76cf97a1186
pmartinh <mh.patricio@gmail.com>
2018-03-08 20:27:26 +0100

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,6 @@ import { GitHubService } from './github.service';
   selector: 'app-root', 
   template: `
   <nav class="navbar navbar-default">
-<<<<<<< HEAD
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
@@ -36,33 +35,6 @@ routerLinkActive="active">GitHub</a></li>
     </div><!-- /.container-fluid -->
     </nav>                                    
     <router-outlet></router-outlet>                                                        
-=======
-  <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-   <button type="button" class="navbar-toggle collapsed" 
-data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
-aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" routerLink="">ngProject</a>
-      </div>
-  <!-- Collect the nav links, forms, and other content for toggling -->
-<div class="collapse navbar-collapse" 
-id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-  <li><a routerLink="" routerLinkActive="active">Home</a></li>
-   <li><a routerLink="GitHub" 
-routerLinkActive="active">GitHub</a></li>
-      </ul>
-      </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-  </nav>                                    
-  <router-outlet></router-outlet>                                                              
->>>>>>> 6c404e2939b30c562bcc89d3e42bd04277567728
     `,
   providers: []
     
